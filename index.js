391da7d345ce3b27cae000faf00a3f03ae716ec9
Alistair MacDonald <al@pwn.io>
2016-02-05 17:33:45 +0000

    Adding key loyout helper graphic.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+ * Dvorak Programmers Tutorial CLI
+ * ===============================
+ * Author: Alistair MacDonald
+ * Original Lessons: http://gigliwood.com/abcd/lessons/
+ * Programmer Dvorak Lessons: http://programmer-dvorak.appspot.com/
+ */
+
 (function () {
 
   'use strict';
@@ -16,7 +24,6 @@
 
   //var lessonNumber = parseInt(process.argv[2]).pad(2);
 
-  var lessonNumber = process.argv[2];
 
   function getLesson (file) {
     var filename = './lessons/lesson-' + file + '.txt';
@@ -31,24 +38,142 @@
       });
     });
   }
+  
+  
+  var f0 = clc;  
+  var f1 = clc.red;
+  var f2 = clc.yellow;
+  var f3 = clc.green;
+  var f4 = clc.cyan;
+  var f5 = clc.magenta;
+  var tb = clc;
 
-  getLesson(lessonNumber)
+  
+  var layout = [
+    // 'MacBook Pro Split Key Layout',
+    '┌───┬───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┬───────┐',
+    '│~ $│% &│[ 7│{ 5│} 3│( 1│= 9│   │* 0│) 2│+ 4│] 6│! 8│` #│ Bkspc │',             
+    '├───┼───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤',
+    '│Tab│; :│, <│. >│ P │ Y │ F │   │ G │ C │ R │ L │/ ?│@ ^│  \\ |  │',       
+    '├───┴───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤',
+    '│Cap/Esc│ A │ O │ E │ U │ I │   │ D │ H │ T │ N │ S │- _│ Enter │',       
+    '├───────┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┴───────┤',
+    '│ Shift │\' "│ Q │ J │ K │ X │   │ B │ M │ W │ V │ Z │   Shift   │',       
+    '├───┬───┼───┼───┼───┴───┴───┴───┴───┴───┴───┼───┴───┼───────────┤',
+    '│fn │Ctl│Alt│Cmd│         Space Bar         │  Cmd  │    Alt    │',       
+    '└───┴───┴───┴───┴───────────────────────────┴───────┴───────────┘'
+  ];
+  
+  var keyboard = layout.join('\n')
+
+    // Finger 1 Left Hand
+    .replace(/\( 1/,   f1('( 1'))
+    .replace(/ Y /,    f1(' Y '))
+    .replace(/ U /,    f1(' U '))
+    .replace(/ K /,    f1(' K '))
+
+    // Finger 1 Right Hand
+    .replace(/\) 2/,  f1(') 2'))
+    .replace(/ C /,   f1(' C '))
+    .replace(/ H /,   f1(' H '))
+    .replace(/ M /,   f1(' M '))
+
+    // Finger 0 Left Hand
+    .replace(/\= 9/,  f0('= 9'))
+    .replace(/ F /,   f0(' F '))
+    .replace(/ I /,   f0(' I '))
+    .replace(/ X /,   f0(' X '))
+
+    // Finger 0 Right Hand
+    .replace(/\* 0/,   f0('* 0'))
+    .replace(/ G /,    f0(' G '))
+    .replace(/ D /,    f0(' D '))
+    .replace(/ B /,    f0(' B '))
+
+    // Finger 2 Left Hand
+    .replace(/\} 3/,   f2('} 3'))
+    .replace(/ P /,    f2(' P '))
+    .replace(/ E /,    f2(' E '))
+    .replace(/ J /,    f2(' J '))
+
+    // Finger 2 Right Hand
+    .replace(/\+ 4/,   f2('+ 4'))
+    .replace(/ R /,    f2(' R '))
+    .replace(/ T /,    f2(' T '))
+    .replace(/ W /,    f2(' W '))
+
+    // Finger 3 Left Hand
+    .replace(/\{ 5/,   f3('{ 5'))
+    .replace(/\. \>/,  f3('. >'))
+    .replace(/ O /,    f3(' O '))
+    .replace(/ Q /,    f3(' Q '))
+    .replace(/ Cmd /,  f3(' Cmd '))
+
+    // Finger 3 Right Hand
+    .replace(/\] 6/,   f3('} 6'))
+    .replace(/ L /,    f3(' L '))
+    .replace(/ N /,    f3(' N '))
+    .replace(/ V /,    f3(' V '))
+    .replace(/Cmd/,    f3('Cmd'))
+    
+    // Finger 4 Left Hand
+    .replace(/\[ 7/,   f4('{ 5'))
+    .replace(/\, </,   f4(', <'))
+    .replace(/ A /,    f4(' A '))
+    .replace(/' "/,    f4('\' "'))
+    .replace(/Alt/,    f4('Alt'))
+    
+    // Finger 4 Right Hand
+    .replace(/\! 8/,   f4('! 8'))
+    .replace(/\/ \?/,  f4('/ ?'))
+    .replace(/ S /,    f4(' S '))
+    .replace(/ Z /,    f4(' Z '))
+   
+    // Finger 5 Left Hand
+    .replace(/\% \&/,    f5('% &'))
+    .replace(/\; \:/,    f5('\\ :'))
+    .replace(/Cap\/Esc/, f5('Cap\/Esc'))
+    .replace(/ Shift /,  f5(' Shift '))
+
+    // Finger 5 Right Hand
+    .replace(/\` #/,   f5('` #'))
+    .replace(/\@ \^/,  f5('@ ^'))
+    .replace(/- _/,    f5('- _'))
+    .replace(/  Shift  /,    f5('  Shift  '))
+    .replace(/   Alt   /,    f5('   Alt   '))
+    .replace(/Ctl/,    f5('Crl'))
+    
+    .replace(/Space Bar/, tb('Space Bar'));
+
+  console.log(keyboard);
+
+  
+  var lessonFile = process.argv[2];
+   
+  getLesson(lessonFile)
   .then(function (lesson) {
     beginLesson(lesson);
   }).catch(function () {
     console.log(clc.red('Could not load lesson!'));
   });
 
+
+
+
   var fail = clc.bgRed.white;
   var pass = clc.bgGreen.black;
   var complete = clc.green.underline;
   var white = clc.white;
   var green = clc.green;
-  var title = clc.white.underline;
+  var title = clc.underline;
   
   function beginLesson (lesson) {
     var lines = lesson.split('\n');
-    console.log('\n' + title(lines[0]));  
+    
+    // Log the lesson title
+    console.log('\n' + title(lines[0]));
+    
+    // Remove the lesson title from the lesson lines
     lines.shift();
     var cursorLine = 0;
     var currentLine;
@@ -98,41 +223,46 @@
     keypress(process.stdin);
     process.stdin.on('keypress', function (ch, key) {
       //console.log(key.name);
+      //console.log(ch);
 
-      if (key && key.ctrl && key.name == 'c') {
-        process.stdin.pause();
-      }
+      if (key && key.hasOwnProperty('name')) {
+        if (key && key.ctrl && key.name == 'c') {
+          process.stdin.pause();
+        }
 
-      if (key.name === 'return') {
-        return;
-      }
+        if (key.name === 'return') {
+          return;
+        }
 
-      if (key.name === 'tab') {
-        return;
-      }
-     
-      if (key.name === 'backspace') {
-        if (cursorColumn > 0) {
-          process.stdout.write(clc.move.left(1));
-          process.stdout.write(clc.erase.lineRight);
-          cursorColumn -= 1;
-          if (trace[cursorColumn] === '0') {
-            lineErrors-=1;
+        if (key.name === 'tab') {
+          return;
+        }
+       
+        if (key.name === 'backspace') {
+          if (cursorColumn > 0) {
+            process.stdout.write(clc.move.left(1));
+            process.stdout.write(clc.erase.lineRight);
+            cursorColumn -= 1;
+            if (trace[cursorColumn] === '0') {
+              lineErrors-=1;
+            }
+            trace = trace.substr(0, trace.length-1);
           }
-          trace = trace.substr(0, trace.length-1);
+          
+          return;
         }
-      } else {
-        if (ch === currentLine[cursorColumn]) {
-          process.stdout.write(pass(ch));
-          trace += '1';
-        } else {
-          process.stdout.write(fail(ch));
-          lineErrors += 1;
-          totalErrors += 1;
-          trace += '0';
-        }
-        cursorColumn += 1;
       }
+
+      if (ch === currentLine[cursorColumn]) {
+        process.stdout.write(pass(ch));
+        trace += '1';
+      } else {
+        process.stdout.write(fail(ch));
+        lineErrors += 1;
+        totalErrors += 1;
+        trace += '0';
+      }
+      cursorColumn += 1;
 
       if (cursorColumn === currentLine.length && !lineErrors) {
         nextLine();
