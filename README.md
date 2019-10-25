06ce3f825a2372267ea456d4ddf552db15b90e31
F1LT3R <al@pwn.io>
2019-10-25 02:16:56 -0400

    Update to version 2. Add menus and Kata.

--- README.md
+++ README.md
@@ -1,66 +1,76 @@
-# "Programmers Dvorak" CLI Tutorial
+# Dvorak Programmers CLI Tutorial 2.0
 
 A command line typing tutor for the [Programmers
 Dvorak](http://www.kaufmann.no/roland/dvorak/)
 Dvorak keyboard layout.
 
-![Dvorak typing tutor screenshot](http://i.imgur.com/SNac5To.png)
-
-
 ## Installation
 
-The recommendation is to install the tutorial
-globally with `npm install -g` so that you may run
-the program from any directory.
+NPM
 
 ```shell
 npm install -g dvorak-programmers-tutorial
 ```
 
+Yarn
+
+```shell
+yarn add --global dvorak-programmers-tutorial
+```
 
 ## Usage
 
-**Running a lesson**
+Type `dvorak` to load the main-menu.
 
-To run the first lesson type: `dvorak 01`
+![Dvorak Programmers Tutorial Main Menu](https://i.imgur.com/2OBkfq9.png)
 
-**Listing all lessons**
+### Keyboard Layout
 
-To list a lesson type: `dvorak -l`
+View the split keyboard layout.
 
-![Dvorak tutorial lesson list](http://i.imgur.com/015TZwn.png)
+![Dvorak Programmers Tutorial Keyboard Layout](https://i.imgur.com/HQSDtIX.png)
 
-**Keyboard layout**
+### Lessons
 
-To show the Dvorak keyboard layout type: `dvorak
--k`
+Choose a Lesson.
 
-```
-┌───┬───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┬───────┐
-│~ $│% &│{ 5│{ 5│} 3│( 1│= 9│   │* 0│) 2│+ 4│} 6│! 8│` #│ Bkspc │
-├───┼───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤
-│Tab│\ :│, <│. >│ P │ Y │ F │   │ G │ C │ R │ L │/ ?│@ ^│  \ |  │
-├───┴───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤
-│Cap/Esc│ A │ O │ E │ U │ I │   │ D │ H │ T │ N │ S │- _│ Enter │
-├───────┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┴───────┤
-│ Shift │' "│ Q │ J │ K │ X │   │ B │ M │ W │ V │ Z │   Shift   │
-├───┬───┼───┼───┼───┴───┴───┴───┴───┴───┴───┼───┴───┼───────────┤
-│fn │Crl│Alt│Cmd│         Space Bar         │  Cmd  │    Alt    │
-└───┴───┴───┴───┴───────────────────────────┴───────┴───────────┘
-```
+![Dvorak Choose Typing Lesson](https://i.imgur.com/iGfcvES.png)
 
-**Help**
+Begin typing!
 
-To show the Help menu type: `dvorak` or `dvorak
--h`
+![Dvorak Choose Typing Lesson](https://i.imgur.com/ENSBeqK.png)
 
+- If it's good - its green. If it's bad - its red.
+- _You have to hit enter at the end of each line._
 
-## Errors
+Your score will be waiting for you at the end of the lesson.
 
-This is how errors look.
+![Dvorak words per minute and error count statistics at end of lesson](https://i.imgur.com/zvQW4sn.png)
 
-![Dvorak typing lesson error screenshot](http://i.imgur.com/pz5Bq2i.png)
+## Katas
 
+Typing Dvorak is not about going fast - its about going good. So stop trying so hard.
+
+Grab a beer, or something better. Sit back in your comfy chair. Spin up the slow tunes, put your favorite audio book on - and ease in to the joy of Dvorak.
+
+Your fingers will thank you.
+
+### Tao Te Ching
+
+![Dvorak Programmers Kata - Tao Te Ching](https://i.imgur.com/gjjUUPj.png)
+
+> Tao Te Ching (Chinese: 道德經 [ Listen (help. · info)]) is the Chinese name of a book by a man named Laozi (or Lao Tzu, which literally means "old master"). Laozi was a sage (a wise man) and he was a record-keeper. The title can mean "The Book of the Way and its Virtue."
+> &mdash; [Wikipedia](https://simple.wikipedia.org/wiki/Tao_Te_Ching)
+
+> The Tao-te Ching presented a way of life intended to restore harmony and tranquillity to a kingdom racked by widespread disorders.
+> &mdash; [Britanica](https://www.britannica.com/topic/Tao-te-Ching)
+
+### The Proverbs of King Solomon
+
+![The Dvorak Programmers Kata - The Proverbs of King Solomon](https://i.imgur.com/0aAGIB5.png)
+
+> [Proverbs] is an example of the Biblical wisdom tradition, and raises questions of values, moral behavior, the meaning of human life, and right conduct.
+> &mdash; [Wikipedia](https://en.wikipedia.org/wiki/Book_of_Proverbs)
 
 ## Links
 
@@ -71,8 +81,10 @@ This is how errors look.
 - [Enhanced Programmers Dvorak Lessons](http://programmer-dvorak.appspot.com/)
 - [Keyboard Layout Analyzer](http://patorjk.com/keyboard-layout-analyzer/v2/)
 
-### Recommended Keyboards
+### Recommended Dvorak Keyboards
 
+- [Aterus](https://atreus.technomancy.us/)
+- [Aterus62](https://shop.profetkeyboards.com/product/atreus62-keyboard)
 - [Infinity Ergodox](https://www.massdrop.com/buy/infinity-ergodox)
 - [TypeMatrix](http://typematrix.com/)
 
--- index.js
+++ index.js
@@ -1,316 +1,412 @@
 #!/usr/bin/env node
 
 /*
- * Dvorak Programmers Tutorial CLI
- * ===============================
- * Author: Alistair MacDonald
- * Original Lessons: http://gigliwood.com/abcd/lessons/
- * Programmer Dvorak Lessons: http://programmer-dvorak.appspot.com/
- */
+* Dvorak Programmers Tutorial CLI
+* ===============================
+* Author: Alistair MacDonald
+* Original Lessons: http://gigliwood.com/abcd/lessons/
+* Programmer Dvorak Lessons: http://programmer-dvorak.appspot.com/
+*/
 
-(function () {
+const keypress = require('keypress');
+const clc = require('cli-color');
+const fs = require('fs');
+const tty = require('tty');
+const program = require('commander');
+const readline = require('readline');
+const {prompt, Select} = require('enquirer');
+const pkg = require('./package.json');
 
-  'use strict';
-  
-  var keypress = require('keypress');
-  var clc = require('cli-color');
-  var Promise = require('bluebird');
-  var fs = require('fs');
-  var tty = require('tty');
-  var program = require('commander');
-  var pkg = require('./package.json');
-
-  var lessonsPath = '/lessons';
-  var lessonID;
-
-  program
-    .version(pkg.version)
-    .usage('[options] <lesson_id>')
-    .option('-l, --list', 'List of lessons')
-    .option('-k, --keyboard', 'Show Programmers Dvorak keyboard layout')
-    .action(function (lesson_id) {
-      lessonID = lesson_id;
-    });
-   
-  program.parse(process.argv);
-
-  if (!lessonID) {
-    program.outputHelp();
+const kataMeta = [
+  {
+    name: 'Tao Te Ching',
+    type: 'tao-te-ching',
+    dir: '/kata/tao-te-ching',
+    description: 'The Tao Te Ching of Lao-tzu',
+    url: 'http://www.with.org/tao_te_ching_en.pdf'
+  },
+  {
+    name: 'Proverbs',
+    description: 'The Proverbs of King Solomon',
+    type: 'proverbs',
+    dir: '/kata/proverbs',
+    url: 'https://ebible.org/engwebp/'
   }
+];
+
+const title = fs.readFileSync(__dirname + '/title.ascii', 'utf8');
+const lessonsPath = '/lessons';
+let TOC;
+let fileToLoad;
+
+program
+.version(pkg.version)
+.usage('[options] <my-file.txt>')
+.action(input => {
+  fileToLoad = input;
+});
+
+program.parse(process.argv);
+
+function drawKeyboard () {
+  const c0 = clc.red;  
+  const c1 = clc.white;  
+  const c3 = clc.yellow;
+  const c4 = clc.green;    
+  const c5 = clc.blue;
   
-  program.keyboard && drawKeyboard();
-  program.list && listLessons();
-  lessonID && runLesson(lessonID);
+  const layout = [
+    '┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐',
+    '│\' "│, <│. >│ P │ Y │   │ F │ G │ C │ R │ L │',       
+    '├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤',
+    '│ A │ O │ E │ U │ I │   │ D │ H │ T │ N │ S │',       
+    '├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤',
+    '│; :│ Q │ J │ K │ X │   │ B │ M │ W │ V │ Z │',       
+    '└───┴───┴───┴───┴───┘   └───────────────────┘'
+  ];
+  
+  const keyboard = layout.join('\n')
+    .replace(/ Y /,    c0(' Y '))
+    .replace(/ I /,    c0(' I '))
+    .replace(/ X /,    c0(' X '))
+    .replace(/ F /,    c0(' F '))
+    .replace(/ D /,    c0(' D '))
+    .replace(/ B /,    c0(' B '))
 
-  function listLessons () {
-    var path = __dirname + lessonsPath;
-    var files = fs.readdirSync(path);
-
-    var promises = [];
-    files.forEach(function (file, n) {
-      var filename = path + '/' + file;
-      promises.push(loadLesson(filename));
-    });
-    Promise.all(promises).then(values => {
-      var titles = values.map(lines => {
-        var title = lines[0];
-        var id = title.split('Lesson ')[1].split(':')[0];
-        return title.replace(id, clc.green(id));
-      });
-      titles.sort();
-      titles.forEach(title => { console.log(title);});
-    });
-  }
-
-  function drawKeyboard () {
-
-    var f0 = clc;  
-    var f1 = clc.red;
-    var f2 = clc.yellow;
-    var f3 = clc.green;
-    var f4 = clc.cyan;
-    var f5 = clc.magenta;
-    var tb = clc;
+    .replace(/ P /,    c1(' P '))
+    .replace(/ U /,    c1(' U '))
+    .replace(/ K /,    c1(' K '))
+    .replace(/ G /,    c1(' G '))
+    .replace(/ H /,    c1(' H '))
+    .replace(/ M /,    c1(' M '))
     
-    var layout = [
-      // 'MacBook Pro Split Key Layout',
-      '┌───┬───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┬───────┐',
-      '│~ $│% &│[ 7│{ 5│} 3│( 1│= 9│   │* 0│) 2│+ 4│] 6│! 8│` #│ Bkspc │',             
-      '├───┼───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤',
-      '│Tab│; :│, <│. >│ P │ Y │ F │   │ G │ C │ R │ L │/ ?│@ ^│  \\ |  │',       
-      '├───┴───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┼───────┤',
-      '│Cap/Esc│ A │ O │ E │ U │ I │   │ D │ H │ T │ N │ S │- _│ Enter │',       
-      '├───────┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┴───────┤',
-      '│ Shift │\' "│ Q │ J │ K │ X │   │ B │ M │ W │ V │ Z │   Shift   │',       
-      '├───┬───┼───┼───┼───┴───┴───┴───┴───┴───┴───┼───┴───┼───────────┤',
-      '│fn │Ctl│Alt│Cmd│         Space Bar         │  Cmd  │    Alt    │',       
-      '└───┴───┴───┴───┴───────────────────────────┴───────┴───────────┘'
-    ];
-    
-    var keyboard = layout.join('\n')
-      // Finger 1 Left Hand
-      .replace(/\( 1/,   f1('( 1'))
-      .replace(/ Y /,    f1(' Y '))
-      .replace(/ U /,    f1(' U '))
-      .replace(/ K /,    f1(' K '))
-      // Finger 1 Right Hand
-      .replace(/\) 2/,   f1(') 2'))
-      .replace(/ C /,    f1(' C '))
-      .replace(/ H /,    f1(' H '))
-      .replace(/ M /,    f1(' M '))
-      // Finger 0 Left Hand
-      .replace(/\= 9/,   f0('= 9'))
-      .replace(/ F /,    f0(' F '))
-      .replace(/ I /,    f0(' I '))
-      .replace(/ X /,    f0(' X '))
-      // Finger 0 Right Hand
-      .replace(/\* 0/,   f0('* 0'))
-      .replace(/ G /,    f0(' G '))
-      .replace(/ D /,    f0(' D '))
-      .replace(/ B /,    f0(' B '))
-      // Finger 2 Left Hand
-      .replace(/\} 3/,   f2('} 3'))
-      .replace(/ P /,    f2(' P '))
-      .replace(/ E /,    f2(' E '))
-      .replace(/ J /,    f2(' J '))
-      // Finger 2 Right Hand
-      .replace(/\+ 4/,   f2('+ 4'))
-      .replace(/ R /,    f2(' R '))
-      .replace(/ T /,    f2(' T '))
-      .replace(/ W /,    f2(' W '))
-      // Finger 3 Left Hand
-      .replace(/\{ 5/,   f3('{ 5'))
-      .replace(/\. \>/,  f3('. >'))
-      .replace(/ O /,    f3(' O '))
-      .replace(/ Q /,    f3(' Q '))
-      .replace(/ Cmd /,  f3(' Cmd '))
-      // Finger 3 Right Hand
-      .replace(/\] 6/,   f3('} 6'))
-      .replace(/ L /,    f3(' L '))
-      .replace(/ N /,    f3(' N '))
-      .replace(/ V /,    f3(' V '))
-      .replace(/Cmd/,    f3('Cmd'))
-      // Finger 4 Left Hand
-      .replace(/\[ 7/,   f4('{ 5'))
-      .replace(/\, </,   f4(', <'))
-      .replace(/ A /,    f4(' A '))
-      .replace(/' "/,    f4('\' "'))
-      .replace(/Alt/,    f4('Alt'))
-      // Finger 4 Right Hand
-      .replace(/\! 8/,   f4('! 8'))
-      .replace(/\/ \?/,  f4('/ ?'))
-      .replace(/ S /,    f4(' S '))
-      .replace(/ Z /,    f4(' Z '))
-      // Finger 5 Left Hand
-      .replace(/\% \&/,     f5('% &'))
-      .replace(/\; \:/,     f5('\\ :'))
-      .replace(/Cap\/Esc/,  f5('Cap\/Esc'))
-      .replace(/ Shift /,   f5(' Shift '))
-      // Finger 5 Right Hand
-      .replace(/\` #/,      f5('` #'))
-      .replace(/\@ \^/,     f5('@ ^'))
-      .replace(/- _/,       f5('- _'))
-      .replace(/  Shift  /, f5('  Shift  '))
-      .replace(/   Alt   /, f5('   Alt   '))
-      .replace(/Ctl/,       f5('Crl'))
-      .replace(/Space Bar/, tb('Space Bar'));
+    .replace(/\. \>/,  c3('. >'))
+    .replace(/ E /,    c3(' E '))
+    .replace(/ J /,    c3(' J '))
+    .replace(/ C /,    c3(' C '))
+    .replace(/ T /,    c3(' T '))
+    .replace(/ W /,    c3(' W '))
 
-    console.log(keyboard);
-  };
+    .replace(/\, </,   c4(', <'))
+    .replace(/ O /,    c4(' O '))
+    .replace(/ Q /,    c4(' Q '))
+    .replace(/ R /,    c4(' R '))
+    .replace(/ N /,    c4(' N '))
+    .replace(/ V /,    c4(' V '))
 
-  function loadLesson (filename) {
-    return new Promise (function (resolve, reject) {
-      fs.readFile(filename, 'utf8', function (err, data) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data.split('\n'));
-      });
+    .replace(/ L /,    c5(' L '))
+    .replace(/ S /,    c5(' S '))
+    .replace(/ Z /,    c5(' Z '))
+    .replace(/' "/,    c5('\' "'))
+    .replace(/ A /,    c5(' A '))
+    .replace(/\; \:/,  c5('\\ :'))
+
+  console.log(keyboard);
+};
+
+const firstLineOf = file => new Promise((resolve, reject) => {
+  const lineReader = readline.createInterface({
+    input: require('fs').createReadStream(file)
+  });
+
+  lineReader.on('line', line => {
+    lineReader.close();
+    resolve(line)
+  });
+
+  lineReader.on('error', e => {
+    reject(error);
+  });
+});
+
+const getLeadLines = dir => new Promise((resolve, reject) => {
+  const path = __dirname + dir;
+  const files = fs.readdirSync(path).sort();
+  const paths = []; 
+  const getFirstLines = [];
+
+  files.forEach(function (file, n) {
+    const filename = path + '/' + file;
+    paths.push(filename);
+    getFirstLines.push(firstLineOf(filename));
+  });
+
+  return Promise.all(getFirstLines)
+    .then(firstLines => {
+      const leadLines = files.map((file, idx) => ({
+        path: paths[idx],
+        leadLine: firstLines[idx]
+      }));
+      resolve(leadLines);
+    })
+    .catch(reject);
+});
+
+const transformLeads = (leadLines, type) => new Promise((resolve, reject) => {
+  try {
+    const lessons = leadLines.map((file, index) => {
+      const title = file.leadLine;
+      const lessonDef = {title, path: file.path};
+      return lessonDef;
     });
+    resolve(lessons);
+  } catch (error) {
+    reject(error);
   }
+});
 
-  function loadTextFile (filename) {
+const getLessons = () => new Promise((resolve, reject) => {
+  getLeadLines(lessonsPath)
+    .then(lessons => transformLeads(lessons, 'Lesson'))
+    .then(lessons => resolve(lessons))
+    .catch(error => console.error(error));
+});
 
-  }
 
-  function runLesson (lesson_id) {  
-    var lessonFile = __dirname + lessonsPath + '/lesson-' + lesson_id + '.txt';     
-    loadLesson(lessonFile)
-    .then(function (lesson) {
-      beginLesson(lesson);
-    }).catch(function (error) {
-      
-      var lessonFile = lessonID;
-      loadLesson(lessonFile)
-      .then(function (lesson) {
-        beginLesson(lesson);
-      })
-      .catch(function (error) {
-        console.warn(clc.red('Could not load lesson!'));
-        console.error(error);
-      })
-    });
-  }
+const getKatas = () => new Promise((resolve, reject) => {
+  const promises = [];
   
-  var fail = clc.bgRed.white;
-  var pass = clc.bgGreen.black;
+  kataMeta.forEach(kata => {
+    promises.push(new Promise((resolve, reject) => {
+      getLeadLines(kata.dir)
+        .then(leadLines => transformLeads(leadLines))
+        .then(katas => resolve({[kata.type]: katas}));
+    }));
+  });
+  
+  Promise.all(promises).then(results => {
+    const katas = {};
+    results.forEach(kata => {
+      const key = Reflect.ownKeys(kata)[0];
+      katas[key] = kata[key];
+    });
+    resolve(katas);
+  }).catch(error => console.error(error));
+});
+
+const loadLesson = filename => {
+  const content = fs.readFileSync(filename, 'utf8')
+  const lines = content.split('\n');
+  return lines;
+};
+  
+const start = file => {
+  const lines = loadLesson(file);
+   beginLesson(lines);
+};
+
+function beginLesson (lines) {
+  var fail = clc.bgRed.white.bold.underline;
+  var pass = clc.green;
   var complete = clc.green.underline;
-  var white = clc.white;
+  var white = clc;
   var green = clc.green;
-  var title = clc.underline;
+  var titleStyle = clc.underline;
+
+  // Log the lesson title
+  const firstLine = `${lines[0]}`;
+  console.log('\n' + titleStyle(firstLine));
   
-  function beginLesson (lines) {
-    
-    // Log the lesson title
-    console.log('\n' + title(lines[0]));    
-    
-    // Remove the lesson title from the lesson lines
-    lines.shift();
+  // Remove the lesson title from the lesson lines
+  lines.shift();
 
-    var cursorLine = 0;
-    var currentLine;
-    var cursorColumn = 0;
-    var lineErrors= 0;
-    var trace = '';
-    var totalErrors = 0;
-    var start = (+ new Date());
+  var cursorLine = 0;
+  var currentLine;
+  var cursorColumn = 0;
+  var lineErrors= 0;
+  var trace = '';
+  var totalErrors = 0;
+  var start = (+ new Date());
 
-    // Load the first line
-    nextLine();
+  // Load the first line
+  nextLine();
 
-    function nextLine () {
-      lineErrors = 0;
-      cursorColumn = 0;
-      trace = '';
-      currentLine = lines[cursorLine];
-      console.log(white('\n' + currentLine));
+  function nextLine () {
+    lineErrors = 0;
+    cursorColumn = 0;
+    trace = '';
+    currentLine = lines[cursorLine];
+    console.log(white('\n' + currentLine));
 
-      if (cursorLine === lines.length-1) {
-        lessonComplete();
-      }
-      cursorLine +=1;     
+    if (cursorLine === lines.length-1) {
+      lessonComplete();
     }
-
-    function lessonComplete () {
-      var end = (+ new Date());
-      var time = end - start;
-      var wordCount = lines.join(' ').split(' ').length - 1;
-      var msPerWord = time / wordCount;
-      var wpm = (60 * 1000 / msPerWord).toFixed(2);
-      
-      console.log(complete('Lesson complete!'));
-      console.log(white('Total errors: ' + totalErrors));
-      console.log(white('Words Per Minute: ' + wpm));
-      console.log();
-      
-      process.stdin.pause();
-    }
-
-    // Begin reading keys
-    keypress(process.stdin);
-
-    process.stdin.on('keypress', function (ch, key) {
-      if (key && key.hasOwnProperty('name')) {
-
-        // Quit on Ctrl+C        
-        if (key && key.ctrl && key.name == 'c') {
-          process.stdin.pause();
-        }
-
-        var returned = key && key.name === 'return' || false;
-
-        // Ignore keys
-        // if (key.name === 'return' || key.name === 'tab') {
-        if ( key.name === 'tab') {
-          return;
-        }
-       
-        if (key.name === 'backspace') {
-          if (cursorColumn > 0) {
-            process.stdout.write(clc.move.left(1));
-            process.stdout.write(clc.erase.lineRight);
-            cursorColumn -= 1;
-            if (trace[cursorColumn] === '0') {
-              lineErrors-=1;
-            }
-            trace = trace.substr(0, trace.length-1);
-          }
-          
-          return;
-        }
-      }
-
-      if (ch === currentLine[cursorColumn]) {
-        if (!returned) {
-          process.stdout.write(pass(ch));
-          trace += '1';
-        }
-      } else {
-        if (!returned) {
-          process.stdout.write('\u0007');
-          process.stdout.write(fail(ch));
-          lineErrors += 1;
-          totalErrors += 1;
-          trace += '0';
-        }
-      }
-      if (!returned) {
-        cursorColumn += 1;
-      }
-
-      // console.log(cursorColumn, currentLine.length, returned);
-      if (cursorColumn >= currentLine.length && !lineErrors && returned) {
-        nextLine();
-      }
-    });
-
-    if (typeof process.stdin.setRawMode == 'function') {
-      process.stdin.setRawMode(true);
-    } else {
-      tty.setRawMode(true);
-    }
-
-    process.stdin.resume();
+    cursorLine +=1;     
   }
 
-})();
+  function lessonComplete () {
+    var end = (+ new Date());
+    var time = end - start;
+    var wordCount = lines.join(' ').split(' ').length - 1;
+    var msPerWord = time / wordCount;
+    var wpm = (60 * 1000 / msPerWord).toFixed(2);
+    
+    console.log(complete('Lesson complete!'));
+    console.log(white('Total errors: ' + totalErrors));
+    console.log(white('Words Per Minute: ' + wpm));
+    console.log();
+    
+    process.stdin.pause();
+  }
+
+  // Begin reading keys
+  keypress(process.stdin);
+
+  process.stdin.on('keypress', function (ch, key) {
+    if (key && key.hasOwnProperty('name')) {
+
+      // Quit on Ctrl+C        
+      if (key && key.ctrl && key.name == 'c') {
+        process.stdin.pause();
+      }
+
+      var returned = key && key.name === 'return' || false;
+
+      // Ignore keys
+      // if (key.name === 'return' || key.name === 'tab') {
+      if ( key.name === 'tab') {
+        return;
+      }
+      
+      if (key.name === 'backspace') {
+        if (cursorColumn > 0) {
+          process.stdout.write(clc.move.left(1));
+          process.stdout.write(clc.erase.lineRight);
+          cursorColumn -= 1;
+          if (trace[cursorColumn] === '0') {
+            lineErrors-=1;
+          }
+          trace = trace.substr(0, trace.length-1);
+        }
+        
+        return;
+      }
+    }
+
+    if (ch === currentLine[cursorColumn]) {
+      if (!returned) {
+        process.stdout.write(pass(ch));
+        trace += '1';
+      }
+    } else {
+      if (!returned) {
+        process.stdout.write('\u0007');
+        process.stdout.write(fail(ch));
+        lineErrors += 1;
+        totalErrors += 1;
+        trace += '0';
+      }
+    }
+    if (!returned) {
+      cursorColumn += 1;
+    }
+
+    // console.log(cursorColumn, currentLine.length, returned);
+    if (cursorColumn >= currentLine.length && !lineErrors && returned) {
+      nextLine();
+    }
+  });
+
+  if (typeof process.stdin.setRawMode == 'function') {
+    process.stdin.setRawMode(true);
+  } else {
+    tty.setRawMode(true);
+  }
+
+  process.stdin.resume();
+}
+
+const generateTOC = () => new Promise((resolve, reject) => {
+  Promise.all([getLessons(), getKatas()])
+  .then(([lessons, katas]) => {
+    resolve({lessons, katas});
+  }).catch(error => console.error(error));
+});
+
+const menu = {
+  main: async toc => {
+    console.log(title);
+
+    const choices = [
+      {
+        message: clc.green('Dvorak Typeing Lessons'),
+        name: 'lessons'
+      }
+    ];
+
+    kataMeta.forEach(kata => {
+      choices.push({
+          name: kata.type,
+          message: clc.blue(kata.description)
+      });
+    });
+
+    choices.push(
+      {
+        message: clc.yellow('Show Dvorak Keyboard Layout'),
+        name: 'keyboard'
+      },{
+        message: clc.red('Show Help'),
+        name: 'help'
+    });
+
+    const prompt = new Select({
+      name: 'value',
+      message: 'Select your experience',
+      choices
+    });
+
+    const answer = await prompt.run();
+    return answer;
+  },
+
+  sub: async list => {
+    const choices = [];
+
+    list.forEach(file => {
+      choices.push({
+          message: file.title,
+          name: file.path
+      });
+    });
+
+    const prompt = new Select({
+      name: 'value',
+      message: 'Choose your beginning',
+      limit: 5,
+      choices
+    });
+
+    const answer = await prompt.run();
+    return answer;
+  },
+
+  help: () => program.outputHelp(),
+
+  keyboard: () => drawKeyboard()
+};
+
+const startWithMenu = () => generateTOC()
+  .then(generatedTOC => {
+    TOC = generatedTOC;
+  })
+  .then(menu.main)
+  .then(type => {
+    if (Reflect.has(menu, type)) {
+      return menu[type]();
+    }
+    
+    const list = type === 'lessons' ? TOC.lessons : TOC.katas[type];
+    
+    menu.sub(list).then(file => {
+      start(file);
+    })
+  });
+
+ 
+const startWithFile = () => {}
+
+program.keyboard && drawKeyboard();
+fileToLoad && 
+  startWithFile(fileToLoad) || 
+  startWithMenu();
